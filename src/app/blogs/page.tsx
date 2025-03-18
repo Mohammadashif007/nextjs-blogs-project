5edbfc07d22db1e102b901183e081fa9e1601744
Mohammad Ashif <mohammadashif510@gmail.com>
2025-03-18 07:08:14 +0600

    data fetch with rtk query done

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,50 +1,25 @@
-// import BlogCard from "@/components/ui/BlogCard";
-// import { Blog } from "@/types";
-// import { Metadata } from "next";
-// import React from "react";
-
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
 import { Blog } from "@/types";
 
-// export const metaData: Metadata = {
-//     title : "NexaBlog | Blogs"
+// export const metadata  = {
+//     title: "NexaBlog | Blogs"
 // }
 
-// const BlogsPage = async () => {
-//     const res = await fetch("http://localhost:5000/blogs", {
-//         cache: "no-store",
-//     });
-//     const blogs = await res.json();
-//     return (
-//         <div className="w-[90%] mx-auto">
-//             <h1 className="text-3xl text-center my-5 font-bold">
-//                 Explore
-//                 <span className="text-teal-600"> All Blogs</span>
-//             </h1>
-//             <p className="text-center text-gray-400 w-2/5 mx-auto">
-//                 <i>
-//                     Dive into the fascinating world of quantum computing, where
-//                     unlocking unprecedented computational power.
-//                 </i>
-//             </p>
-//             <div className="grid grid-cols-3 gap-3 my-10">
-//                 {blogs.map((blog: Blog) => (
-//                     <BlogCard key={blog.id} blog={blog}></BlogCard>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
+const BlogsPage =  () => {
+    // const res = await fetch("http://localhost:5000/blogs", {
+    //     cache: "no-store",
+    // });
+    // const blogs = await res.json();
+    const {data: blogs, isLoading} = useGetBlogsQuery({});
+    console.log(blogs);
 
-// export default BlogsPage;
-
-export const metadata  = {
-    title: "NexaBlog | Blogs"
-}
-
-const BlogsPage = async () => {
-    const res = await fetch("http://localhost:5000/blogs", {cache: "no-store"});
-    const blogs = await res.json();
+    if(isLoading){
+        return <Spinner></Spinner>
+    }
+   
     return (
         <div className="my-10 w-[90%] mx-auto">
             <h1 className="text-3xl text-center my-5 font-bold">
